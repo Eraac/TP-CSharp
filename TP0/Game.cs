1001eb89745bf54cea4941542bd52da73a684f75
Labesse Kévin <k.labesse@aforp.eu>
2015-11-13 00:20:46 +0100

    Création classe de base
    Celle qui lance le jeu

---
+++ TP0/Game.cs
@@ -0,0 +1,20 @@
+﻿using TP0.Entity;
+using TP0.Gameplay;
+using System.Collections.Generic;
+
+namespace TP0
+{
+    class Game
+    {
+        public Game() {}
+
+        public void run()
+        {
+            // TODO 1. Load players
+            List<Player> players = new List<Player>();
+
+            // 2. Run tournament
+            Tournament tournament = new Tournament(players);
+        }
+    }
+}
