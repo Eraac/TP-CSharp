a7ce7b07f1df2fd7ddc6016041572ed9ce873c12
Labesse Kévin <k.labesse@aforp.eu>
2015-11-13 00:21:37 +0100

    Ajout entité pool
    Gère les matchs entre 2 équipes

---
+++ TP0/Gameplay/Pool.cs
@@ -0,0 +1,23 @@
+﻿using TP0.Entity;
+
+namespace TP0.Gameplay
+{
+    class Pool
+    {
+        private const uint NB_MAX_TEAM_PER_POOL = 2;
+        private Team[] _teams;
+
+        public Pool(Team teamA, Team teamB)
+        {
+            this._teams = new Team[NB_MAX_TEAM_PER_POOL];
+            this._teams[0] = teamA;
+            this._teams[1] = teamA;
+        }
+
+        public Team match()
+        {
+            // TODO
+            return this._teams[0];
+        }
+    }
+}
