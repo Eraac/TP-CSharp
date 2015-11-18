ebcaff0ffe037c788e07f27f803484e48c468c84
Labesse Kévin <k.labesse@aforp.eu>
2015-11-19 00:19:40 +0100

    Ajout JsonReade

---
+++ TP0/Loader/LoadPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using TP0.Entity;
+
+namespace TP0.Loader
+{
+    class LoadPlayer
+    {
+        public LoadPlayer() {}
+
+        public List<Player> loadAll()
+        {
+            return new List<Player>();
+        }
+
+        public Player loadOne(string name)
+        {
+            return new Player("coucou", "coucou", "coucou");
+        }
+    }
+}
