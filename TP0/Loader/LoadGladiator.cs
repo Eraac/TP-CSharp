05428c502a929db52d8bd0bd59a8cef7e1c11114
Labesse Kévin <k.labesse@aforp.eu>
2015-11-21 01:04:25 +0100

    Hydrate Gladiator

---
+++ TP0/Loader/LoadGladiator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using TP0.Entity;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace TP0.Loader
+{
+    class GladiatorConverter : CustomCreationConverter<Gladiator>
+    {
+        public override Gladiator Create(Type objectType)
+        {
+            return new Gladiator();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JObject jsonObject = JObject.Load(reader);
+
+            return this.loadGladiator(jsonObject, new Gladiator());
+        }
+
+        private Gladiator loadGladiator(JObject jsonObject, Gladiator gladiator)
+        {
+            gladiator.name = jsonObject.Property("name").ToString();
+            gladiator.nbMatchPlayed = uint.Parse(jsonObject.Property("nb_match_played").Value.ToString());
+            gladiator.nbMatchWon = uint.Parse(jsonObject.Property("nb_match_won").Value.ToString());
+            dynamic weapons = jsonObject.Property("weapons").ToList();
+            dynamic armors = jsonObject.Property("armors").ToList();
+
+            if (weapons.Contains(0)) {
+
+            } else {
+                Console.WriteLine("coucou");
+            }
+
+            // TODO Stuff
+
+            return gladiator;
+        }
+    }
+
+    class LoadGladiator
+    {
+        public LoadGladiator() {}
+
+        public Gladiator loadOne(string name)
+        {
+            // TODO check if file exists
+            string stringJson = File.ReadAllText("Assets/Gladiator/" + name + ".json");
+
+            return JsonConvert.DeserializeObject<Gladiator>(stringJson, new GladiatorConverter());
+        }
+    }
+}
