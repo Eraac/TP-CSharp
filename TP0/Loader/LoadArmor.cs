20aa75c5aa0d4006c7312e07acc35f6b6683b96b
Eraac <kevin@labesse.me>
2015-11-21 16:23:55 +0100

    Implémentation loadStuff

---
+++ TP0/Loader/LoadArmor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Converters;
+using TP0.Entity;
+using TP0.Stuff;
+using TP0.Stuff.Armor;
+using TP0.Loader.Factory;
+
+namespace TP0.Loader
+{
+	public class LoadArmor
+	{
+		public LoadArmor () {}
+
+		public void loadAll (Gladiator gladiator, List<int> armors)
+		{
+			ArmorFactory armorFactory = new ArmorFactory();
+			List<SlotStuff> slots = new List<SlotStuff>{SlotStuff.ARMOR_HEAD, SlotStuff.SHIELD};
+			int index = 0;
+
+			foreach (int idArmor in armors) 
+			{
+				ListArmor armor = (ListArmor) idArmor;
+				Item item = armorFactory.getArmor(armor);
+
+				gladiator.addItem(item, slots[index % slots.Count]);
+
+				index++;
+			}
+		}
+	}
+}
+
---
+++ TP0/Loader/LoadWeapon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using TP0.Entity;
+using TP0.Loader.Factory;
+using TP0.Stuff.Weapon;
+using TP0.Stuff;
+
+namespace TP0.Loader
+{
+	public class LoadWeapon
+	{
+		public LoadWeapon () {}
+
+		public void loadAll (Gladiator gladiator, List<int> weapons)
+		{
+			WeaponFactory weaponFactory = new WeaponFactory();
+			List<SlotStuff> slots = new List<SlotStuff>{SlotStuff.WEAPON_LEFT_HAND, SlotStuff.WEAPON_RIGHT_HAND};
+			int index = 0;
+
+			foreach (int idWeapon in weapons)
+			{
+				ListWeapon weapon = (ListWeapon) idWeapon;
+				Item item = weaponFactory.getWeapon (weapon);
+
+				gladiator.addItem(item, slots[index % slots.Count]);
+
+				index++;
+			}
+		}
+	}
+}
+
