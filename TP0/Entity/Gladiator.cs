7b9f5f36b71c43d70697e49ae430351f8653e64d
Labesse Kévin <k.labesse@aforp.eu>
2015-11-21 01:03:17 +0100

    Changement getter/setter

--- TP0/Entity/Gladiator.cs
+++ TP0/Entity/Gladiator.cs
@@ -17,7 +17,6 @@ namespace TP0.Entity
 				return this._stuff;
 			}
 		}
-
 		public IDebuffAttack debuff 
 		{
 			get {
@@ -27,14 +26,38 @@ namespace TP0.Entity
 				this._debuff = value;
 			}
 		}
+        public string name
+        {
+            get {
+                return this._name;
+            }
+            set {
+                this._name = value;
+            }
+        }
+        public uint nbMatchWon
+        {
+            get {
+                return this._nbMatchWon;
+            }
+            set {
+                this._nbMatchWon = value;
+            }
+        }
+        public uint nbMatchPlayed
+        {
+            get {
+                return this._nbMatchPlayed;
+            }
+            set {
+                this._nbMatchPlayed = value;
+            }
+        }
 
-		public Gladiator (string name, uint nbMatchPlayed = 0, uint nbMatchWon = 0)
+        public Gladiator ()
 		{
-			this._name = name;
 			this._stuff = new Stuff();
 			this._debuff = new NoDebuff();
-			this._nbMatchPlayed = nbMatchPlayed;
-			this._nbMatchWon = 0;
 		}
 
 		public bool addItem (Item item, SlotStuff slotStuff)
@@ -78,6 +101,11 @@ namespace TP0.Entity
 		{
 			return this._stuff.hasWeapon();
 		}
-	}
+
+        public override string ToString()
+        {
+            return "[Gladiator] : " + this._name;
+        }
+    }
 }
 
